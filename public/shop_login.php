fb83e5d6f2f05fee96ea7b3d080db0ac122ba885
Misty Shaine Niones <125946850+msn02@users.noreply.github.com>
2024-06-23 20:01:03 +0800

    feat: customer login for click-and-collect module

---
+++ public/shop_login.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Customer Login</title>
+    <?php include '../views/html_resources.php';?>
+    <link rel="stylesheet" href="../assets/css/shop_login.css">
+</head>
+<body class="bg_white">
+    <div class="bg_dark_blue container-fluid position-absolute">
+        <div class="container text_logo">
+            <p class="m-0 text-white py-3"><strong>CYCLE</strong>SALES</p>
+        </div>
+    </div>
+    <div class="bg_gray bg_gray_width container p-3">
+        <div class="container pt-5 mt-4">
+            <div class="btn_text m-0">
+                <button class="btn btn-secondary pe-4 rounded-1"><i class="bi bi-chevron-left me-2"></i>Back</button>
+            </div>
+            <div class="form_shadow card col-md-3 p-4 mx-auto mt-3 rounded-1 border-0">
+                <form action="" class="py-2">
+                    <div class="bg_white">
+                        <h4 class="text_header_1 m-0 pb-3 text-center">LOG IN</h4>
+                    </div>
+                    <div class="text_input">
+                        <div class="mb-3">
+                            <label for="email" class="form-label">Email</label>
+                            <input type="email" class="form-control rounded-1 focus-ring" id="email" name="email" required>
+                        </div>
+                        <div class="mb-3">
+                            <label for="password" class="form-label">Password</label>
+                            <input type="password" class="form-control rounded-1 focus-ring" id="password" name="password" required>
+
+                            <div class="text_link mb-4 mt-2 text-end">
+                                <p><a href="shop_account_recovery.php">Forgot Password</a></p>
+                            </div>
+                        </div>
+                        <div class="mb-3">
+                            <button type="submit" class="btn btn-primary w-100 rounded-1 p-2">Login</button>
+                        </div>
+                        <div class="text_link mb-3 text-center">
+                            <p>Not a member? <a href="shop_register.php">Create an Account</a></p>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
---
+++ views/html_resources.php
@@ -0,0 +1,18 @@
+<!-- CSS and JavaScript Resources -->
+<link rel="stylesheet" href="../assets/css/main.css">
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Inter:wght@100..900&display=swap" rel="stylesheet">
+
+<!-- <script src="../javascript/nav_bar.js" defer></script>
+<script src="../javascript/dropdown.js" defer></script> -->
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+
+<style>
+    body {
+        font-family: 'Inter', sans-serif;
+    }
+</style>
