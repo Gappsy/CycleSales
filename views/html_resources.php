44b1df8582ff1a5eeb7aa99c4c72f88f3fac434c
Misty Shaine Niones <125946850+msn02@users.noreply.github.com>
2024-06-26 15:19:28 +0800

    chore: added fontawesome library

--- views/html_resources.php
+++ views/html_resources.php
@@ -2,6 +2,7 @@
 <link rel="stylesheet" href="../assets/css/main.css">
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+<link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css">
 <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100..900&display=swap" rel="stylesheet">
@@ -10,6 +11,7 @@
 <script src="../javascript/dropdown.js" defer></script> -->
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+<script src="https://kit.fontawesome.com/032f808566.js" crossorigin="anonymous"></script>
 
 <style>
     body {
